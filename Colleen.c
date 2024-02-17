8e04f404616c332f895d4286684a7cad16173e8f
mfunyu <mfunyu@student.42tokyo.jp>
2024-02-17 02:45:16 +0100

    add: template, meet subject requirements

--- Colleen.c
+++ Colleen.c
@@ -1,10 +1,21 @@
+// :)
 #include <stdio.h>
+
+/*
+	Comment Outside of Functions
+*/
+
+void	print_smiley()
+{
+	printf("// :)");
+}
 
 int	main()
 {
 	/*
-		Comment
+		Comment inside of main
 	*/
-	char *source = "#include <stdio.h>%c%cint%cmain()%c{%c%c/*%c%c%cComment%c%c*/%c%cchar *source = %c%s%c;%c%cprintf(source, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10);%c}";
-	printf(source, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10);
+	char *source = "%c#include <stdio.h>%c%c/*%c%cComment Outside of Functions%c*/%c%cvoid%cprint_smiley()%c{%c%cprintf(%c// :)%c);%c}%c%cint%cmain()%c{%c%c/*%c%c%cComment inside of main%c%c*/%c%cchar *source = %c%s%c;%c%cprint_smiley();%c%cprintf(source, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10, 9, 10);%c}";
+	print_smiley();
+	printf(source, 10, 10, 10, 10, 9, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, source, 34, 10, 9, 10, 9, 10);
 }
---
+++ Colleen_template.c
@@ -0,0 +1,21 @@
+// :)
+#include <stdio.h>
+
+/*
+	Comment Outside of Functions
+*/
+
+void	print_smiley()
+{
+	printf("// :)");
+}
+
+int	main()
+{
+	/*
+		Comment inside of main
+	*/
+	char *source = "";
+	print_smiley();
+	REPLACE
+}
